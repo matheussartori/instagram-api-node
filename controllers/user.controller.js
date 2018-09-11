fc15051e6c6d6ca39c003136668869ded105dd7b
Matheus Sartori <ms.sartori@outlook.com.br>
2018-09-11 09:14:00 -0300

    Syntax fix

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,8 @@
 let logger = require('../services/logger.js');
 
 module.exports = (app) => {
+    let User = mongoose.model('User', UserSchema);
+
     app.get('/users/self', (req, res) => {
         console.log('Get information about the owner of the access_token.');
     });
@@ -10,26 +12,24 @@ module.exports = (app) => {
     });
 
     app.post('/users/new', (req, res) => {
-        let user = new User(
-	        {
-	        	id: req.body.id,
-	        	username: req.body.username,
-	        	full_name: req.body.fullname,
-	        	profile_picture: req.body.profile_picture,
-	        	bio: req.body.bio,
-	        	website: req.body.website,
-	        	is_business: req.body.is_business,
-	        	counts: {
-	        		media: 0,
-	        		follows: 0,
-	        		followed_by: 0
-	        	}
-	        }
-    	);
+        let user = new User({
+        	id: req.body.id,
+        	username: req.body.username,
+        	full_name: req.body.fullname,
+        	profile_picture: req.body.profile_picture,
+        	bio: req.body.bio,
+        	website: req.body.website,
+        	is_business: req.body.is_business,
+        	counts: {
+        		media: 0,
+        		follows: 0,
+        		followed_by: 0
+        	}
+        });
         
         user.save(function (err) {
             if (err) {
-                return next(err);
+                return handleError(err);
             }
             res.send('User created successfully.');
         });
