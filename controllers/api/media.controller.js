0982285be60222c360f2af2517d52601b184a674
Matheus Sartori <ms.sartori@outlook.com.br>
2018-09-27 08:56:29 -0300

    Comment count

--- controllers/api/media.controller.js
+++ controllers/api/media.controller.js
@@ -3,6 +3,7 @@ let logger = require('../../services/logger.js');
 let User = require('../../models/user.model.js');
 let Media = require('../../models/media.model.js');
 let Like = require('../../models/like.model.js');
+let Comment = require('../../models/comment.model.js');
 
 module.exports = app => {
     app.get('/media/search/:id', (req, res) => {
@@ -13,15 +14,25 @@ module.exports = app => {
                 res.status(400).send(err);
             } else {
                 if(media) {
+                    let timeline = media.toObject();
+
                     Like.countDocuments({ media: media._id }, (err, likes) => {
-                        var timeline = media.toObject();
 
                         timeline.likes = {
                             count: likes
                         };
 
-                        res.status(200).send(timeline);
                     });
+
+                    Comment.countDocuments({ media: media._id }, (err, comments) => {
+
+                        timeline.comments = {
+                            count: comments
+                        };
+
+                    });
+
+                    res.status(200).send(timeline);
                 } else {
                     res.status(400).send({error: "Media not found."});
                 }
